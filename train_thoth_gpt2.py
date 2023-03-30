a7dccb19308e24dd9a6a634b01256f3d432e8837
Erik Jespersen <42016062+Mookse@users.noreply.github.com>
2023-03-30 00:24:40 -0400

    loop through train directory Fixes #1

--- train_thoth_gpt2.py
+++ train_thoth_gpt2.py
@@ -3,18 +3,19 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 from transformers import GPT2LMHeadModel, GPT2Tokenizer, GPT2Config, AdamW, get_linear_schedule_with_warmup
 
+
 class CustomTextDataset(Dataset):
-    def __init__(self, tokenizer, file_paths, block_size):
+    def __init__(self, tokenizer, files, block_size):
         self.tokenizer = tokenizer
         self.block_size = block_size
 
         self.tokens = []
 
-        for file_path in file_paths:
+        for file_path in files:
             with open(file_path, "r") as f:
                 text = f.read()
-
-            tokens = tokenizer(text, return_tensors="pt", add_special_tokens=True)["input_ids"].squeeze()
+            tokens = tokenizer(text, return_tensors="pt", add_special_tokens=True)[
+                "input_ids"].squeeze()
             self.tokens.append(tokens)
 
         self.tokens = torch.cat(self.tokens)
@@ -28,14 +29,15 @@ class CustomTextDataset(Dataset):
 
         return self.tokens[start_idx:end_idx]
 
+
 model_name = "gpt2"
 epochs = 4
 learning_rate = 5e-5
 warmup_steps = 1e2
 batch_size = 4
 max_sequence_length = 128
-# Replace with the path to your text corpus
-train_files = ["train/corpus.txt", "train/presentations.txt", "train/transcripts.txt"]
+# Replace with the path to your text corpus directory
+train_dir = "train/"
 # check gpu
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Load tokenizer functionality
@@ -44,6 +46,9 @@ tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 config = GPT2Config.from_pretrained("gpt2")
 # Create GPT-2 model
 model = GPT2LMHeadModel.from_pretrained("gpt2", config=config)
+# Get list of text files in train_dir
+train_files = [os.path.join(train_dir, f)
+               for f in os.listdir(train_dir) if f.endswith('.txt')]
 # Initialize the CustomTextDataset with the tokenizer
 train_dataset = CustomTextDataset(tokenizer, train_files, max_sequence_length)
 train_dataloader = DataLoader(
